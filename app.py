0437881f0eb2efcd5163c71320d3ab34a3085cc4
bayan79 <bayandink@yandex.ru>
2020-08-20 20:30:01 +0500

    [bugfix]

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def registration():
         getData = request.get_data()
         json_params = json.loads(getData) 
         
-        model.predict(vec.transform([json_params['user_message']]).toarray())
+        pred = model.predict(vec.transform([json_params['user_message']]).toarray())
+        resp['category'] = pred
         #напишите прогноз и верните его в ответе в параметре 'prediction'
 
         
