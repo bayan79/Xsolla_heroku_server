04a5185d00e02287ead6e32785db4b61c63ac45f
bayan79 <bayandink@yandex.ru>
2020-08-26 05:51:16 +0500

    Edit main app.py, add notebook + models

--- app.py
+++ app.py
@@ -16,12 +16,16 @@ import json
 from ast import literal_eval
 import traceback
 
+from preprocessor import Preprocessor
+from libherokuserver import clear_message
+
+
 application = Flask(__name__)
 
 
 #загружаем модели из файла
-vec = pickle.load(open("./models/tfidf.pickle", "rb"))
-model = lgb.Booster(model_file='./models/lgbm_model.txt')
+preproc = pickle.load(open("./models/final_preproc.pickle", "rb"))
+model = pickle.load(open("./models/final_model.pickle", "rb"))
 
 
 # тестовый вывод
@@ -33,6 +37,22 @@ def hello():
     
     return response
 
+
+def class_predict(message, preproc, model):
+    """Make prediction of message with ordering
+    message: string
+    preproc: Preprocessor
+    model: ML model with predict_proba func
+    """
+    _order=['afs', 'other', 'ps']
+    vec = preproc.message_prepare(message)
+    pred = model.predict_proba(vec)[0]    
+    
+    results = dict(enumerate(pred))  
+    return [results[preproc.label_map[_class]] for _class in _order]
+
+
+
 # предикт категории
 @application.route("/categoryPrediction" , methods=['GET', 'POST'])  
 def registration():
@@ -43,17 +63,21 @@ def registration():
     try:
         getData = request.get_data()
         json_params = json.loads(getData) 
+        message = json_params['user_message']
         
-        pred = model.predict(vec.transform([json_params['user_message']]).toarray())
-        resp['prediction'] = pred.tolist()
-        #напишите прогноз и верните его в ответе в параметре 'prediction'
+        if not clear_message(message):
+            return {
+                'message':'ok',
+                'category': 'afs',
+                'prediction': [1, 0, 0]
+            }
+            
+        pred = class_predict(message, preproc, model)
+        resp['prediction'] = pred
+        resp['category'] = ['afs', 'other', 'ps'][np.argmax(pred)]
 
-        
-
-        
     except Exception as e: 
-        print(e)
-        resp['message'] = e
+        resp['message'] = "Sorry, we are already working on error!!! Take a rest and drink tea :)"
       
     response = jsonify(resp)
     
